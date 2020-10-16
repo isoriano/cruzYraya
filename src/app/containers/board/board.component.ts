243e025c314749c4eefc8ecf6abc5b3d7a47bc8a
Ivan Soriano <iss@ersl.ie>
2020-10-16 17:35:15 +0200

    Add "dummy" autoMove and re-style

--- src/app/containers/board/board.component.ts
+++ src/app/containers/board/board.component.ts
@@ -1,43 +1,77 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-board',
-  templateUrl: './board.component.html',
-  styleUrls: ['./board.component.scss']
+  selector: "app-board",
+  templateUrl: "./board.component.html",
+  styleUrls: ["./board.component.scss"],
 })
 export class BoardComponent implements OnInit {
-
   squares: any[];
   xIsNext: boolean;
-  winner: string;
+  oScore = 0;
+  xScore = 0;
 
   get player() {
-    return this.xIsNext ? 'x' : 'o';
+    return this.xIsNext ? "x" : "o";
   }
 
-  get xAmount() {
-    return this.squares.filter(value => value === 'x')?.length;
+  set winner(winner: string) {
+    this._winner = winner;
+    if (winner === null) {
+      return;
+    }
+    if (winner === "x") {
+      this.xScore = this.xScore + 1;
+    } else {
+      this.oScore = this.oScore + 1;
+    }
   }
 
-  constructor() { }
+  get winner() {
+    return this._winner;
+  }
+
+  private _winner: string;
+
+  constructor() {}
 
   ngOnInit() {
     this.newGame();
   }
 
-  makeMove(idx: number) {
+  makeMove(idx: number, autoMove: boolean) {
+    if (this.winner || (this.player === "o" && !autoMove)) {
+      return;
+    }
+    const tempPlayer = this.player;
     if (!this.squares[idx]) {
       this.squares.splice(idx, 1, this.player);
       this.xIsNext = !this.xIsNext;
     }
 
     this.winner = this.calculateWinner();
+    if (!this.winner && tempPlayer === "x") {
+      this.autoMove();
+    }
   }
 
   newGame() {
     this.squares = Array(9).fill(null);
     this.winner = null;
     this.xIsNext = true;
+  }
+
+  private autoMove() {
+    // TODO: Make the autoMove smart so it covers possible defeat scenarios
+    const emptySquares = this.calculateEmpty();
+    if (emptySquares.length === 0) {
+      return;
+    }
+    const randomIndex = this.randomSquare(emptySquares.length);
+
+    if (emptySquares[randomIndex] >= 0) {
+      this.makeMove(emptySquares[randomIndex], true);
+    }
   }
 
   private calculateWinner() {
@@ -49,15 +83,34 @@ export class BoardComponent implements OnInit {
       [1, 4, 7],
       [2, 5, 8],
       [0, 4, 8],
-      [2, 4, 6]
+      [2, 4, 6],
     ];
     for (const [a, b, c] of lines) {
       const squareA = this.squares[a];
-      if (squareA && squareA === this.squares[b] && squareA === this.squares[c]) {
+      if (
+        squareA &&
+        squareA === this.squares[b] &&
+        squareA === this.squares[c]
+      ) {
         return squareA;
       }
     }
 
     return null;
+  }
+
+  private calculateEmpty() {
+    const empty = [];
+    for (let index = 0; index < 9; index++) {
+      if (this.squares[index] === null) {
+        empty.push(index);
+      }
+    }
+
+    return empty;
+  }
+
+  private randomSquare(max: number) {
+    return Math.floor(Math.random() * max);
   }
 }
